b0b952a749eae2c5a9fb83148b927e1f34acaab9
kat <kjacasio@hawaii.edu>
2020-02-20 09:34:50 -1000

    App Build One Commit Feb 20

--- AppBuildOnePrt3/ViewController.swift
+++ AppBuildOnePrt3/ViewController.swift
@@ -10,9 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var firstTabLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBAction func ACMchangedButtonPressed(_ sender: Any)
+    {
+       
+        
+        
+        
+        
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+//set firstTabLabel to "University of Hawaii West Oahu"
+        self.firstTabLabel.text = "University of Hawaii West Oahu"
+ //description of University
+        self.descriptionLabel.text = "UHWO Mission Statemen: Since its inception in 1976, the University of Hawaiʻi – West Oʻahu has been committed to delivering accessible, affordable higher education to residents on the Leeward Coast through dynamic, flexible, and responsive methods."
+        
+        
+        
+//set background color to view gray
+        
+    
+        
+//add two buttons labeled as "About UHWO" and "About ACM"
+        
+        
+        
+        
+        
+    
     }
 
 
